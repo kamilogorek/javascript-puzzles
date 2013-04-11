52683c8f71d86b203739d538a27ce9aa2b2a63c1
Kamil Ogórek <kamil.ogorek@gmail.com>
2013-04-11 03:36:15 +0300

    Puzzle 2 answer

--- 2.js
+++ 2.js
@@ -3,12 +3,12 @@
 
 // loops 10 times from 0 - 1000 in increments of 100
 for (var i = 0; i < 1000; i += 100) {
-  waitFor(i, function then() {
-    console.log(i)
-  })
+  waitFor(i);
 }
 
 // this will run a callback function after waiting milliseconds
-function waitFor(milliseconds, callback) {
-  setTimeout(callback, milliseconds)
+function waitFor(milliseconds) {
+  setTimeout(function () {
+    console.log(milliseconds);
+  }, milliseconds)
 }
